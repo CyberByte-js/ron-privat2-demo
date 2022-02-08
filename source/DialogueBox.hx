80ced9e5e0b80756db9387c9c1fb63e1ed751269
CyberByte-js <71409005+CyberByte-js@users.noreply.github.com>
2022-02-08 11:44:51 +0044

    i forgor x6

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -261,21 +261,6 @@ class DialogueBox extends FlxSpriteGroup
 			emote = true;
 			box.y = 315;
 		}
-		else if (StringTools.contains(dialogueList[0], 'sussy'))
-		{
-			dialogueList[0] = dialogueList[0].substr(5).trim();
-			FlxG.sound.play(Paths.sound('among'), 1);
-		}
-		else if (StringTools.contains(dialogueList[0], 'static'))
-		{
-			dialogueList[0] = dialogueList[0].substr(6).trim();
-			FlxG.sound.play(Paths.sound('static'), 1);
-		}
-		else if (StringTools.contains(dialogueList[0], 'vine'))
-		{
-			dialogueList[0] = dialogueList[0].substr(4).trim();
-			FlxG.sound.play(Paths.sound('vine'), 1);
-		}
 		else
 		{
 			box.y = 375;
